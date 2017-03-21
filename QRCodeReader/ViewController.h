1ec678fd8d61cb904ccfc42b74e954d9dc172811
Hemant Saini <hemantsaini@Hemants-MacBook-Pro.local>
2017-03-21 15:36:54 +0530

    Initial Commit

---
+++ QRCodeReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCodeReader
+//
+//  Created by Hemant Saini on 21/03/17.
+//  Copyright © 2017 Hemant Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QRCodeReader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QRCodeReader
+//
+//  Created by Hemant Saini on 21/03/17.
+//  Copyright © 2017 Hemant Saini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
